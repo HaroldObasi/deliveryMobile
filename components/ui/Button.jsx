8039a87ae7f90ba2ec7e538e5c91dbfaa19f057b
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-17 19:32:11 +0100

    create order page and custom button component

---
+++ components/ui/Button.jsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from "react-native";
+import { Button } from "react-native-paper";
+import React from "react";
+
+const Button = ({ children, buttonColor, textColor }) => {
+  return (
+    <View>
+      <Text>Button</Text>
+    </View>
+  );
+};
+
+export default Button;
+
+const styles = StyleSheet.create({});
