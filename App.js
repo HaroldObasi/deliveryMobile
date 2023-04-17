f6ad24b74c0cafe1840f134c041661d1446ef76c
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-17 12:14:27 +0100

    created screens for Profile and Home

--- App.js
+++ App.js
@@ -2,16 +2,20 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Login from "./screens/Login";
 import Signup from "./screens/Signup";
+import Home from "./screens/Home";
+import Profile from "./screens/Profile";
 import { readObjectFromCache } from "./utiils/caching";
 
 export default function App() {
   const Stack = createNativeStackNavigator();
+  const [user, setUser] = useState(null);
 
   const getData = async () => {
-    const user = await readObjectFromCache("user");
+    const res = await readObjectFromCache("user");
+    setUser(res);
     if (user !== null) {
       console.log("user is present: ", user);
     } else {
@@ -25,18 +29,36 @@ export default function App() {
 
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Signup"
-          component={Signup}
-          options={{ headerShown: false }}
-        />
-      </Stack.Navigator>
+      {user === null ? (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Login"
+            component={Login}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name="Signup"
+            component={Signup}
+            options={{ headerShown: false }}
+          />
+        </Stack.Navigator>
+      ) : (
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Home"
+            component={Home}
+            options={{ headerShown: false }}
+          />
+
+          <Stack.Screen
+            name="Profile"
+            component={Profile}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      )}
     </NavigationContainer>
   );
 }
