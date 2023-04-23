3fc8c425898f5f04fdb368c0f04eb7a603f35562
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-23 19:18:26 +0100

    Auto complete finally worked

--- screens/CreateOrder/index.jsx
+++ screens/CreateOrder/index.jsx
@@ -1,14 +1,36 @@
 import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from "../../private";
+import { theme } from "../../styles/theme";
 
 const CreateOrder = () => {
+  const [pickupPoint, setUpPickupPoint] = useState({});
+
   return (
-    <View>
-      <Text>CreateOrder</Text>
-    </View>
+    <GooglePlacesAutocomplete
+      placeholder="Pickup"
+      minLength={2}
+      onFail={(err) => console.error(err)}
+      fetchDetails={true}
+      listViewDisplayed="auto"
+      onPress={(data, details = true) => {
+        console.log("i am here", data, details);
+      }}
+      query={{
+        key: GOOGLE_API_KEY,
+        language: "en",
+      }}
+    />
   );
 };
 
 export default CreateOrder;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  heading: {
+    color: theme.colors.text.header,
+    fontSize: theme.font.size.md,
+    marginBottom: 3,
+  },
+});
---
+++ styles/theme.js
@@ -0,0 +1,34 @@
+export const theme = {
+  colors: {
+    primary: {
+      main: "#0652DD",
+      light: "#ACC5F4",
+      dark: "#0544B8",
+      deep: "#03296F",
+    },
+    grey: {
+      100: "#DCDCDC",
+      200: "#C5C5C5",
+      300: "#8A8A8A",
+    },
+    text: {
+      body: "#48484A",
+      header: "#1C1C1E",
+    },
+    success: {
+      main: "#0FB56D",
+      light: "#E5FCF6",
+      dark: "#26704C",
+    },
+  },
+  font: {
+    size: {
+      xs: 10,
+      sm: 15,
+      md: 20,
+      lg: 25,
+      xl: 30,
+      xxl: 35,
+    },
+  },
+};
