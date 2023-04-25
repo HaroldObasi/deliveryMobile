b82d1443f7b69afbb2e85b81dcb95c2f5e9eb4e1
HaroldObasi <haroldobasi2k16@gmail.com>
2023-04-25 04:33:33 +0100

    order item navigates to new order detail screen

--- components/ui/OrderItem.jsx
+++ components/ui/OrderItem.jsx
@@ -1,10 +1,15 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { theme } from "../../styles/theme";
+import { useNavigation } from "@react-navigation/native";
 import React from "react";
 
 const OrderItem = ({ item }) => {
+  const navigation = useNavigation();
   return (
-    <TouchableOpacity style={styles.orderItem}>
+    <TouchableOpacity
+      style={styles.orderItem}
+      onPress={() => navigation.navigate("OrderDetails", item)}
+    >
       <Text style={styles.text}>From: {item.pickupPoint.shortName}</Text>
       <Text style={styles.text}>To: {item.destination.shortName} </Text>
       <Text style={styles.text}>
---
+++ screens/OrderDetails/index.jsx
@@ -0,0 +1,80 @@
+import { StyleSheet, Text, View } from "react-native";
+import { API_URL } from "../../apiConfig";
+import React, { useEffect, useState } from "react";
+import { theme } from "../../styles/theme";
+import { useGlobalContext } from "../../context";
+import OrderQuotes from "./OrderQuotes";
+
+const OrderDetails = ({ route }) => {
+  const { user } = useGlobalContext();
+  const [orderQuotes, setOrderQuotes] = useState([]);
+  const [quotesLoading, setQuotesLoading] = useState(true);
+  const data = route.params;
+
+  const fetchOrderQuotes = async () => {
+    try {
+      const response = await API_URL.get(`quote/quotesBy/${data._id}`);
+      console.log("quotes response: ", response.data);
+      setOrderQuotes(response.data);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setQuotesLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchOrderQuotes();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.hStack}>
+        <Text>
+          <Text style={{ color: theme.colors.primary.main }}>From: </Text>
+          {data.pickupPoint.shortName}
+        </Text>
+        <Text>
+          {" "}
+          <Text style={{ color: theme.colors.primary.main }}>To: </Text>{" "}
+          {data.destination.shortName}
+        </Text>
+      </View>
+      {user.role === "USER" ? (
+        data.assignedCourier._id === null ? (
+          <Text style={{ marginVertical: 5 }}>No couriers assigned</Text>
+        ) : (
+          <Text style={{ marginVertical: 5 }}>
+            Assigned courier: {data.assignedCourier.fullName}
+          </Text>
+        )
+      ) : (
+        <>a courier</>
+      )}
+
+      <View>
+        <Text>Client Name: {data.recipientName}</Text>
+        <Text>Client Number: {data.recipientNumber}</Text>
+      </View>
+
+      <Text style={{ fontSize: theme.font.size.md, marginVertical: 5 }}>
+        Description: {data.description}
+      </Text>
+
+      <OrderQuotes quotes={orderQuotes} quotesLoading={quotesLoading} />
+    </View>
+  );
+};
+
+export default OrderDetails;
+
+const styles = StyleSheet.create({
+  container: {
+    marginTop: theme.font.size.xl,
+    marginHorizontal: theme.font.size.sm,
+  },
+  hStack: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+  },
+});
