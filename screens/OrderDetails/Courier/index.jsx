847cd6d870d12c5d44d21116f2784c7be100fdcf
HaroldObasi <haroldobasi2k16@gmail.com>
2023-05-25 22:22:24 +0100

    style changes

--- screens/OrderDetails/Courier/index.jsx
+++ screens/OrderDetails/Courier/index.jsx
@@ -1,23 +1,56 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, ScrollView } from "react-native";
 import React, { useState } from "react";
 import GeneralOrderInfo from "../GeneralOrderInfo";
 import { theme } from "../../../styles/theme";
 import QuoteForm from "./QuoteForm";
 import { hasUserQuotedOrder } from "./helpers";
 import { useGlobalContext } from "../../../context";
+import { useNavigation } from "@react-navigation/native";
 import UserMap from "../UserMap";
 
 const CourierOrderDetails = ({ orderDetails, orderQuotes, quotesLoading }) => {
-  const [modalVisible, setModalVisible] = useState(false);
   const { user } = useGlobalContext();
   console.log("order details: ", orderDetails);
   return (
-    <View style={styles.container}>
+    <ScrollView style={styles.container}>
       <GeneralOrderInfo order={orderDetails} />
 
       {hasUserQuotedOrder(orderQuotes, user) ? (
         orderDetails?.assignedCourier?._id === user._id ? (
-          <UserMap />
+          orderDetails.delivered === true ? (
+            <View
+              style={{
+                backgroundColor: theme.colors.success.light,
+                paddingVertical: 20,
+                paddingHorizontal: 10,
+                borderRadius: 8,
+                marginVertical: 10,
+              }}
+            >
+              <Text
+                style={{
+                  fontSize: 15,
+                  color: theme.colors.success.dark,
+                }}
+              >
+                You have successfully delivered this order, Congrats !
+              </Text>
+            </View>
+          ) : (
+            <UserMap
+              region={{
+                origin: {
+                  longitude: orderDetails.pickupPoint.long,
+                  latitude: orderDetails.pickupPoint.lat,
+                },
+                destination: {
+                  longitude: orderDetails.destination.long,
+                  latitude: orderDetails.destination.lat,
+                },
+              }}
+              orderDetails={orderDetails}
+            />
+          )
         ) : (
           <View
             style={{
@@ -41,7 +74,7 @@ const CourierOrderDetails = ({ orderDetails, orderQuotes, quotesLoading }) => {
       ) : (
         <QuoteForm orderDetails={orderDetails} />
       )}
-    </View>
+    </ScrollView>
   );
 };
 
@@ -49,7 +82,7 @@ export default CourierOrderDetails;
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: theme.font.size.xl,
+    paddingTop: 10,
     marginHorizontal: 10,
   },
   centeredView: {
