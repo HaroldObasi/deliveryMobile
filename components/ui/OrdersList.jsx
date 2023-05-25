576c1c474d264b0768edf0a53390a50b46bea117
HaroldObasi <haroldobasi2k16@gmail.com>
2023-05-25 22:17:47 +0100

    shows if orders list is empty

--- components/ui/OrdersList.jsx
+++ components/ui/OrdersList.jsx
@@ -7,12 +7,34 @@ const OrdersList = ({ orders, title }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.heading}>{title}</Text>
-      <FlatList
-        data={orders}
-        horizontal
-        renderItem={({ item }) => <OrderItem item={item} />}
-        showsHorizontalScrollIndicator={false}
-      />
+
+      {orders.length > 0 ? (
+        <FlatList
+          data={orders}
+          horizontal
+          renderItem={({ item }) => <OrderItem item={item} />}
+          showsHorizontalScrollIndicator={false}
+        />
+      ) : (
+        <View
+          style={{
+            backgroundColor: theme.colors.error.light,
+            padding: 10,
+            borderRadius: 8,
+            marginVertical: 5,
+            marginHorizontal: 10,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 15,
+              color: theme.colors.error.dark,
+            }}
+          >
+            No orders :(
+          </Text>
+        </View>
+      )}
     </View>
   );
 };
@@ -21,8 +43,7 @@ export default OrdersList;
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: theme.font.size.xl,
-    // marginHorizontal: theme.font.size.sm,
+    marginBottom: theme.font.size.xl,
   },
   heading: {
     fontSize: theme.font.size.sm,
