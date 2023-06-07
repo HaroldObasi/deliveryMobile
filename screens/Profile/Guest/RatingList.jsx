da443db17fe5de35afc64863c0f213c5fb5ed0d0
HaroldObasi <haroldobasi2k16@gmail.com>
2023-06-07 16:35:15 +0100

    styling

--- screens/Profile/Guest/RatingList.jsx
+++ screens/Profile/Guest/RatingList.jsx
@@ -5,6 +5,7 @@ import RatingItem from "./RatingItem";
 const RatingList = ({ ratings }) => {
   return (
     <View>
+      <Text style={{ marginBottom: 5 }}>Ratings</Text>
       {ratings?.length === 0 ? (
         <Text>No ratings have been made for this User</Text>
       ) : (
